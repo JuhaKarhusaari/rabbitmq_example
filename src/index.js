6940260e8f14f1e10d2b8427346a04f7230cabc8
Juha Karhusaari <juha.karhusaari@gmail.com>
2020-10-13 05:33:40 +0300

    fixed connect functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function (error0, connection) { });
-
-amqp.connect('amqp://localhost', function (error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function (error1, channel) { });
-});
-
-amqp.connect('amqp://localhost', function (error0, connection) {
+// Hyper-V Ubuntu Docker RabbitMQ Server => 192.168.100.42
+amqp.connect('amqp://192.168.100.42', function (error0, connection) {
     if (error0) {
         throw error0;
     }
@@ -17,19 +9,19 @@ amqp.connect('amqp://localhost', function (error0, connection) {
         if (error1) {
             throw error1;
         }
-        var queue = 'hello';
-        var msg = 'Hello world';
+
+        let queue = 'hello';
+        let msg = "Hello World!, Here's Johnny !";
 
         channel.assertQueue(queue, {
             durable: false
         });
-
         channel.sendToQueue(queue, Buffer.from(msg));
+
         console.log(" [x] Sent %s", msg);
     });
+    setTimeout(function () {
+        connection.close();
+        process.exit(0);
+    }, 500);
 });
-
-setTimeout(function () {
-    connection.close();
-    process.exit(0)
-}, 500);
--- src/receive.js
+++ src/receive.js
@@ -1,6 +1,7 @@
 const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function (error0, connection) {
+// Hyper-V Ubuntu Docker RabbitMQ Server => 192.168.100.42
+amqp.connect('amqp://192.168.100.42', function (error0, connection) {
     if (error0) {
         throw error0;
     }
@@ -13,12 +14,13 @@ amqp.connect('amqp://localhost', function (error0, connection) {
         channel.assertQueue(queue, {
             durable: false
         });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function (msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {
+            noAck: true
+        });
     });
 });
 
-console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-channel.consume(queue, function (msg) {
-    console.log(" [x] Received %s", msg.content.toString());
-}, {
-    noAck: true
-});
